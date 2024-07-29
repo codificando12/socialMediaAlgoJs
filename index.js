102153996c3a2c0a9d43439544d445ea47d0334d
codificando12 <planeta.chompi18@gmail.com>
2024-07-29 10:20:38 +0800

    challenged finished

--- index.js
+++ index.js
@@ -35,15 +35,33 @@ function signin () {
     const username = newUsername.value;
     const password = newPassword.value;
     const repassword = newRePassword.value;
-    
+    const error = document.createElement("div");
+
     for (let i = 0; i < users.length; i++) {
+        
         if (users[i].name === username) {
-            const existUser = document.createElement("div");
-            existUser.textContent = "Choose another user name";
-            siginForm.appendChild(existUser);
+            error.textContent = "Choose another user name";
+            siginForm.appendChild(error);
+            break;
+        } else if (password !== repassword ) {
+            error.textContent = "Password has to match";
+            siginForm.appendChild(error);
+            break;   
+        } else {
+            const newUser = new User(username, password);
+            users.push(newUser);
+            newUsername.value = "";
+            newPassword.value = "";
+            newRePassword.value = "";
+            error.textContent = "User created, go to log in";
+            siginForm.appendChild(error)
+            break;
         }
     }
+    console.log(users)
 }
+
+
 
 const form = document.querySelector("form");
 const usernameInput = document.querySelector("#username");
@@ -53,12 +71,26 @@ const loginButton = document.querySelector(".login-button");
 const siginForm = document.querySelector(".sigin-form");
 const newUsername = document.querySelector("#new-username");
 const newPassword = document.querySelector("#new-password");
-const newRePassword = document.querySelector("#new-repassword")
+const newRePassword = document.querySelector("#new-repassword");
 const signinButton = document.querySelector(".signin-button");
 
 let users = [];
 
-users.push(new User("juan", "hola"))
+users.push(new User("juan", "hola"));
+if (loginButton) {
+    loginButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        login();
+    });
+}
 
-loginButton.addEventListener("click", login)
-signinButton.addEventListener("click", signin)
+if (signinButton) {
+    signinButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        signin();
+    });
+
+}
+
+
+
